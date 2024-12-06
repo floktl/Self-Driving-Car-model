a64f588bf255b9808cde8d05f61d21977a632654
Santosh Dhal <78152449+sdhal@users.noreply.github.com>
2024-12-06 18:51:55 +0100

    Steering control from Keyboard using Arduino + Raspberry
    We were are able to control the Servo Motor movement from Keyboard using Arduino + Raspberry.

--- Arduino/Steering/Steering.ino
+++ Arduino/Steering/Steering.ino
@@ -1,35 +1,48 @@
 #include <Servo.h>
 
 Servo myservo;
+int pos = 90;  // Position of the servo set to Center.
+int pin12 = 12;
+int pin11 = 11;
+int leftPin = 3;  // Arduino pin for left signal
+int rightPin = 2; // Arduino pin for right signal
 
-int pos = 0;
-
-int pin1=12;
-int pin2=11;
-
-void setup(){
- pinMode(pin1,OUTPUT);
- pinMode(pin2,OUTPUT);
- myservo.attach(9);
- 
-  
- }
-void loop()
-{
-  digitalWrite(pin1,HIGH);
-  digitalWrite(pin2,LOW);
-  for (pos = 0; pos <= 70; pos += 1)
-  {
-    myservo.write(pos);
-    delay(5);
-  }
-  delay(300);
-  for (pos = 70; pos >= 0; pos -= 1)
-  {
-    myservo.write(pos);
-    delay(5);
-  }
-  delay(300);
+void setup() {
+  pinMode(pin12, OUTPUT);
+  pinMode(pin11, OUTPUT);
+  pinMode(leftPin, INPUT);  // Set leftPin as input
+  pinMode(rightPin, INPUT); // Set rightPin as input
+  myservo.attach(9);  // Attach the servo to pin 9
+  Serial.begin(9600);  // Start serial communication at 9600 baud rate
+  Serial.println("Arduino is ready.");
 }
 
-
+void loop() 
+{
+  int leftState = digitalRead(leftPin);   // Read the state of leftPin
+  int rightState = digitalRead(rightPin); // Read the state of rightPin
+  digitalWrite(pin12,HIGH);
+  digitalWrite(pin11,LOW);
+  if (leftState == HIGH) {  // If the left signal is HIGH, move the servo left
+    Serial.print("\nLeft");
+    if ( pos >= 60)
+    {
+      Serial.println(pos);
+      pos -= 10;  // Prevent the position from going below 0
+      myservo.write(pos);     // Update the servo position
+      delay(100);  // Small delay for smooth movement
+    }
+  }
+  if (rightState == HIGH) { // If the right signal is HIGH, move the servo right
+    Serial.print("\nRight");
+    
+    if ( pos <= 120)
+    {
+      Serial.println(pos);
+      pos += 10; // Prevent the position from going above 180
+      myservo.write(pos);      // Update the servo position
+      delay(100);  // Small delay for smooth movement
+    }
+  }
+  //delay(100);  // Optional delay to prevent too much serial data output
+}
