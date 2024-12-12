0e6b804b9bccab27c9e44cafbb506162e297931a
Florian Keitel <147641602+floktl@users.noreply.github.com>
2024-12-12 18:38:23 +0100

    added pinout image

--- Arduino/Steering/Steering.ino
+++ Arduino/Steering/Steering.ino
@@ -1,48 +1,48 @@
-#include <Servo.h>
+// #include <Servo.h>
 
-Servo myservo;
-int pos = 90;  // Position of the servo set to Center.
-int pin12 = 12;
-int pin11 = 11;
-int leftPin = 3;  // Arduino pin for left signal
-int rightPin = 2; // Arduino pin for right signal
+// Servo myservo;
+// int pos = 90;  // Position of the servo set to Center.
+// int pin12 = 12;
+// int pin11 = 11;
+// int leftPin = 3;  // Arduino pin for left signal
+// int rightPin = 2; // Arduino pin for right signal
 
-void setup() {
-  pinMode(pin12, OUTPUT);
-  pinMode(pin11, OUTPUT);
-  pinMode(leftPin, INPUT);  // Set leftPin as input
-  pinMode(rightPin, INPUT); // Set rightPin as input
-  myservo.attach(9);  // Attach the servo to pin 9
-  Serial.begin(9600);  // Start serial communication at 9600 baud rate
-  Serial.println("Arduino is ready.");
-}
+// void setup() {
+//   pinMode(pin12, OUTPUT);
+//   pinMode(pin11, OUTPUT);
+//   pinMode(leftPin, INPUT);  // Set leftPin as input
+//   pinMode(rightPin, INPUT); // Set rightPin as input
+//   myservo.attach(9);  // Attach the servo to pin 9
+//   Serial.begin(9600);  // Start serial communication at 9600 baud rate
+//   Serial.println("Arduino is ready.");
+// }
 
-void loop() 
-{
-  int leftState = digitalRead(leftPin);   // Read the state of leftPin
-  int rightState = digitalRead(rightPin); // Read the state of rightPin
-  digitalWrite(pin12,HIGH);
-  digitalWrite(pin11,LOW);
-  if (leftState == HIGH) {  // If the left signal is HIGH, move the servo left
-    Serial.print("\nLeft");
-    if ( pos >= 60)
-    {
-      Serial.println(pos);
-      pos -= 10;  // Prevent the position from going below 0
-      myservo.write(pos);     // Update the servo position
-      delay(100);  // Small delay for smooth movement
-    }
-  }
-  if (rightState == HIGH) { // If the right signal is HIGH, move the servo right
-    Serial.print("\nRight");
+// void loop() 
+// {
+//   int leftState = digitalRead(leftPin);   // Read the state of leftPin
+//   int rightState = digitalRead(rightPin); // Read the state of rightPin
+//   digitalWrite(pin12,HIGH);
+//   digitalWrite(pin11,LOW);
+//   if (leftState == HIGH) {  // If the left signal is HIGH, move the servo left
+//     Serial.print("\nLeft");
+//     if ( pos >= 60)
+//     {
+//       Serial.println(pos);
+//       pos -= 10;  // Prevent the position from going below 0
+//       myservo.write(pos);     // Update the servo position
+//       delay(100);  // Small delay for smooth movement
+//     }
+//   }
+//   if (rightState == HIGH) { // If the right signal is HIGH, move the servo right
+//     Serial.print("\nRight");
     
-    if ( pos <= 120)
-    {
-      Serial.println(pos);
-      pos += 10; // Prevent the position from going above 180
-      myservo.write(pos);      // Update the servo position
-      delay(100);  // Small delay for smooth movement
-    }
-  }
-  //delay(100);  // Optional delay to prevent too much serial data output
-}
+//     if ( pos <= 120)
+//     {
+//       Serial.println(pos);
+//       pos += 10; // Prevent the position from going above 180
+//       myservo.write(pos);      // Update the servo position
+//       delay(100);  // Small delay for smooth movement
+//     }
+//   }
+//   //delay(100);  // Optional delay to prevent too much serial data output
+// }
--- Arduino/Steering/Steering_cleanup.ino
+++ Arduino/Steering/Steering_cleanup.ino
@@ -1,127 +1,127 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Steering.ino                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: Florian Keitel <fl.keitelgmail.com>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/09 18:21:44 by Florian Kei       #+#    #+#             */
-/*   Updated: 2024/12/09 18:57:52 by Florian Kei      ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   Steering.ino                                       :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: Florian Keitel <fl.keitelgmail.com>        +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2024/12/09 18:21:44 by Florian Kei       #+#    #+#             */
+// /*   Updated: 2024/12/09 18:57:52 by Florian Kei      ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
 
-//-----------------------------------------------------------------------------
-//-------------------- Steering control for arduino ---------------------------
-//-----------------------------------------------------------------------------
+// //-----------------------------------------------------------------------------
+// //-------------------- Steering control for arduino ---------------------------
+// //-----------------------------------------------------------------------------
 
-//------------------------------- libraries -----------------------------------
-#include <Servo.h>
+// //------------------------------- libraries -----------------------------------
+// #include <Servo.h>
 
-Servo myservo;
+// Servo myservo;
 
-// Pin declarations for the Servo and Distance Sensor
-int pos = 90;  // Position of the servo set to Center.
-int pin12 = 12;
-int pin11 = 11;
-int leftPin = 3;  // Arduino pin for left signal
-int rightPin = 2; // Arduino pin for right signal
+// // Pin declarations for the Servo and Distance Sensor
+// int pos = 90;  // Position of the servo set to Center.
+// int pin12 = 12;
+// int pin11 = 11;
+// int leftPin = 3;  // Arduino pin for left signal
+// int rightPin = 2; // Arduino pin for right signal
 
-// Pins for Distance Sensor (HC-SR04)
-const int trigPin = 6;  // TRIG pin connected to Arduino Pin 6
-const int echoPin = 7;  // ECHO pin connected to Arduino Pin 7
+// // Pins for Distance Sensor (HC-SR04)
+// const int trigPin = 6;  // TRIG pin connected to Arduino Pin 6
+// const int echoPin = 7;  // ECHO pin connected to Arduino Pin 7
 
-//	variables for max steering positions
-const unsigned int min_left = 50;
-const unsigned int max_right = 130;
-const unsigned int steer_speed = 10;
-const unsigned int servo_delay_time = 10;
+// //	variables for max steering positions
+// const unsigned int min_left = 50;
+// const unsigned int max_right = 130;
+// const unsigned int steer_speed = 10;
+// const unsigned int servo_delay_time = 10;
 
-// Variables for Distance Sensor
-long duration;
-int distance;
+// // Variables for Distance Sensor
+// long duration;
+// int distance;
 
-//------------------------------- setup -----------------------------------
+// //------------------------------- setup -----------------------------------
 
-void setup()
-{
-	pinMode(pin12, OUTPUT);
-	pinMode(pin11, OUTPUT);
-	pinMode(leftPin, INPUT);  // Set leftPin as input
-	pinMode(rightPin, INPUT); // Set rightPin as input
-	myservo.attach(9);  // Attach the servo to pin 9
-	Serial.begin(9600);  // Start serial communication at 9600 baud rate
-	Serial.println("Arduino is ready.");
+// void setup()
+// {
+// 	pinMode(pin12, OUTPUT);
+// 	pinMode(pin11, OUTPUT);
+// 	pinMode(leftPin, INPUT);  // Set leftPin as input
+// 	pinMode(rightPin, INPUT); // Set rightPin as input
+// 	myservo.attach(9);  // Attach the servo to pin 9
+// 	Serial.begin(9600);  // Start serial communication at 9600 baud rate
+// 	Serial.println("Arduino is ready.");
 
-	// Set up Distance Sensor
-	pinMode(trigPin, OUTPUT);
-	pinMode(echoPin, INPUT);
-}
+// 	// Set up Distance Sensor
+// 	pinMode(trigPin, OUTPUT);
+// 	pinMode(echoPin, INPUT);
+// }
 
-void move_servo(int value)
-{
-  digitalWrite(pin12,HIGH);
-	digitalWrite(pin11,LOW);
-	pos += steer_speed;
-	myservo.write(pos);
-	delay(servo_delay_time);
-}
+// void move_servo(int value)
+// {
+//   digitalWrite(pin12,HIGH);
+// 	digitalWrite(pin11,LOW);
+// 	pos += steer_speed;
+// 	myservo.write(pos);
+// 	delay(servo_delay_time);
+// }
 
-int distance_sensor(int leftState, int rightState)
-{
-	// Read Distance Sensor
-	// Send a LOW pulse to TRIG to ensure it starts in a clean state
-	digitalWrite(trigPin, LOW);
-	delayMicroseconds(2);
+// int distance_sensor(int leftState, int rightState)
+// {
+// 	// Read Distance Sensor
+// 	// Send a LOW pulse to TRIG to ensure it starts in a clean state
+// 	digitalWrite(trigPin, LOW);
+// 	delayMicroseconds(2);
 
-	// Send a HIGH pulse for 10 microseconds to trigger the sensor
-	digitalWrite(trigPin, HIGH);
-	delayMicroseconds(10);
-	digitalWrite(trigPin, LOW);
+// 	// Send a HIGH pulse for 10 microseconds to trigger the sensor
+// 	digitalWrite(trigPin, HIGH);
+// 	delayMicroseconds(10);
+// 	digitalWrite(trigPin, LOW);
 
-	// Read the time it takes for the echo to return
-	duration = pulseIn(echoPin, HIGH);
+// 	// Read the time it takes for the echo to return
+// 	duration = pulseIn(echoPin, HIGH);
 
-	// Calculate the distance in centimeters
-  // (speed of sound is ~343 meters/second or 0.0344 cm/microsecond)
-	distance = duration * 0.0344 / 2;
+// 	// Calculate the distance in centimeters
+//   // (speed of sound is ~343 meters/second or 0.0344 cm/microsecond)
+// 	distance = duration * 0.0344 / 2;
 
-	// Print the distance to the Serial Monitor
-	Serial.print("Distance: ");
-	Serial.print(distance);
-	Serial.println(" cm");
+// 	// Print the distance to the Serial Monitor
+// 	Serial.print("Distance: ");
+// 	Serial.print(distance);
+// 	Serial.println(" cm");
 
-	// If the distance is less than 10 cm, automatically turn left
-	if (leftState != HIGH && rightState != HIGH && distance < 10 && distance > 0)
-	{
-    if (pos + steer_speed > min_left)
-    {
-      move_servo(+steer_speed);
-    }
-    return (1);
-	}
-	return (0);
-}
-void loop()
-{
-	// safety check, to avoid mechanical damage
-	if (pos > max_right || pos < min_left)
-		return;
-	// Read the state of the left and right signals from Raspberry Pi
-  //  steering from raspberry has highest priority due to safety reasons
-	int leftState = digitalRead(leftPin);   // Read the state of leftPin
-	int rightState = digitalRead(rightPin); // Read the state of rightPin
+// 	// If the distance is less than 10 cm, automatically turn left
+// 	if (leftState != HIGH && rightState != HIGH && distance < 10 && distance > 0)
+// 	{
+//     if (pos + steer_speed > min_left)
+//     {
+//       move_servo(+steer_speed);
+//     }
+//     return (1);
+// 	}
+// 	return (0);
+// }
 
-	if (distance_sensor(leftState, rightState) == 0)
-	{
-    if (leftState == HIGH) //move the servo left
-		{
-			if (pos - steer_speed > min_left)
-				move_servo(-steer_speed);
-		}
-		else if (rightState == HIGH) //move the servo right
-		{
-			if (pos + steer_speed < max_right)
-				move_servo(+steer_speed);
-		}
-	}
-}
+// void loop() {
+// 	// safety check, to avoid mechanical damage
+// 	if (pos > max_right || pos < min_left)
+// 		return;
+// 	// Read the state of the left and right signals from Raspberry Pi
+//   //  steering from raspberry has highest priority due to safety reasons
+// 	int leftState = digitalRead(leftPin);   // Read the state of leftPin
+// 	int rightState = digitalRead(rightPin); // Read the state of rightPin
+
+// 	if (distance_sensor(leftState, rightState) == 0)
+// 	{
+//     if (leftState == HIGH) //move the servo left
+// 		{
+// 			if (pos - steer_speed > min_left)
+// 				move_servo(-steer_speed);
+// 		}
+// 		else if (rightState == HIGH) //move the servo right
+// 		{
+// 			if (pos + steer_speed < max_right)
+// 				move_servo(+steer_speed);
+// 		}
+// 	}
+// }
