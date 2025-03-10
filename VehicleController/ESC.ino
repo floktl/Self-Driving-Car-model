f258746ad0350df8e7ce83f3dfbc989124127111
Graham Stronge <graham.stronge@gmail.com>
2025-03-10 14:06:58 +0100

    fixed so the car can drive forwards and backwards and steer left and right

--- VehicleController/ESC.ino
+++ VehicleController/ESC.ino
@@ -6,30 +6,61 @@
 /*   By: gstronge <gstronge@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 23:06:04 by Florian Kei       #+#    #+#             */
-/*   Updated: 2025/03/03 16:30:10 by gstronge         ###   ########.fr       */
+/*   Updated: 2025/03/07 17:48:29 by gstronge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ESC.h"
 
 // ESCMotor constructor
-ESCMotor::ESCMotor(int motorPin) : pin(motorPin), speed(1700) {}
+ESCMotor::ESCMotor(int motorPin) : pin(motorPin), speed(1500) {}
 
 void ESCMotor::initialize()
 {
+	Serial.begin(9600);
 	motor.attach(pin);
-	int temp_speed = speed;
-	while (temp_speed > 1300)
-	{
-		motor.writeMicroseconds(temp_speed);
-		delay(100); // Allow the ESC to
-		temp_speed--;
-	}
+	motor.writeMicroseconds(speed);
+	Serial.println("delay 1 = BEFORE");
+	delay(2000);
+	Serial.println("delay 1 = AFTER");
+	// motor.writeMicroseconds(1000);
+	// delay(2000);
+	// motor.writeMicroseconds(1500);
+	// int temp_speed = speed;
+	// while (temp_speed < 1800)
+	// {
+	// 	Serial.println("1st loop: ");
+	// 	Serial.println(temp_speed);
+	// 	motor.writeMicroseconds(temp_speed);
+	// 	delay(100); // Allow the ESC to
+	// 	temp_speed++;
+	// }
+	// motor.writeMicroseconds(1500);
+	// Serial.println("delay 2 = BEFORE");
+	// delay(10000);
+	// Serial.println("delay 2 = AFTER");
+	// while (temp_speed > 1100)
+	// {
+	// 	Serial.println("2nd loop: ");
+	// 	Serial.println(temp_speed);
+	// 	motor.writeMicroseconds(temp_speed);
+	// 	delay(100); // Allow the ESC to
+	// 	temp_speed--;
+	// }
+	// Serial.println("delay 3 = BEFORE");
+	// delay(10000);
+	// Serial.println("delay 3 = AFTER");
+	// while (temp_speed < 1500)
+	// {
+	// 	motor.writeMicroseconds(temp_speed);
+	// 	delay(100); // Allow the ESC to
+	// 	temp_speed++;
+	// }
 }
 
 void ESCMotor::setSpeed(int newSpeed)
 {
-	speed = constrain(newSpeed, 1300, 1700);
+	speed = constrain(newSpeed, 1100, 1700);
 	motor.writeMicroseconds(speed);
 }
 
